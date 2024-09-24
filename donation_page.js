a1edebb619724b467c84b12eb556ecf66854a4b8
RafatH0ssain <rafat.click.hossain@gmail.com>
2024-09-24 03:11:52 -0300

    deleted history, added javascript, added popu, designed buttons

--- donation_page.js
+++ donation_page.js
@@ -8,3 +8,53 @@ tailwind.config = {
       }
     }
   }
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+
+    // Function to be repeatedly used
+    function donation_handler(buttonId, amountId, totalId) {
+      document.getElementById(buttonId).addEventListener('click', function() {
+        const amount_donated = parseInt(document.getElementById(amountId).value);
+        const total_element = document.getElementById(totalId);
+        const total_amount = parseInt(total_element.textContent.replace(/,/g, ''));
+        
+        if(amount_donated<=0 || isNaN(amount_donated)) {
+          document.getElementById('popup-box').showModal();
+          return;
+        }
+        const new_total = total_amount + amount_donated;
+        total_element.textContent = new_total.toLocaleString();
+        let account_balance = parseInt(document.getElementById('available-balance').innerText.replace(/,/g, ''));
+        if(account_balance - amount_donated < 0) {
+          document.getElementById('popup-box-2').showModal();
+          return;
+        }
+        account_balance -= amount_donated;
+        document.getElementById('available-balance').innerText = account_balance.toLocaleString();
+        document.getElementById('popup-box-3').showModal();
+        return;
+        
+      });
+    }
+  
+    donation_handler('noakhali-button', 'noakhali-amount', 'noakhali-amount-total');
+    donation_handler('feni-button', 'feni-amount', 'feni-amount-total');
+    donation_handler('quota-button', 'quota-amount', 'quota-amount-total');
+
+
+    document.getElementById('history-button').addEventListener('click', function() {
+      document.getElementById('card-container').classList.add('hidden');
+      document.getElementById('history-button').classList.add('bg-FundFlow-green');
+      document.getElementById('history-button').classList.remove('border-2');
+      document.getElementById('donation-button').classList.remove('bg-FundFlow-green');
+      document.getElementById('donation-button').classList.add('border-2', 'border-black');
+    });
+    document.getElementById('donation-button').addEventListener('click', function() {
+      document.getElementById('card-container').classList.remove('hidden');
+      document.getElementById('history-button').classList.remove('bg-FundFlow-green');
+      document.getElementById('history-button').classList.add('border-2');
+      document.getElementById('donation-button').classList.add('bg-FundFlow-green');
+      document.getElementById('donation-button').classList.remove('border-2', 'border-black');
+    });
+  });
